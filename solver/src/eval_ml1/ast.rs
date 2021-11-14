62ca6d9f97f1c4c175615bdf0a6483ea11f6e2ba
akthrms <akt.hrms@gmail.com>
2021-11-15 00:16:19 +0900

    update

--- solver/src/eval_ml1/ast.rs
+++ solver/src/eval_ml1/ast.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Clone, Debug, PartialEq, PartialOrd)]
 pub enum Expression {
     Integer(i64),
@@ -7,4 +9,24 @@ pub enum Expression {
     Sub(Box<Expression>, Box<Expression>),
     Mul(Box<Expression>, Box<Expression>),
     LessThan(Box<Expression>, Box<Expression>),
+}
+
+impl fmt::Display for Expression {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use self::Expression::*;
+
+        match self {
+            Integer(n) => write!(f, "{}", n),
+            Boolean(b) => write!(f, "{}", b),
+            If(expression1, expression2, expression3) => write!(
+                f,
+                "if {} then {} else {}",
+                expression1, expression2, expression3
+            ),
+            Add(left, right) => write!(f, "{} + {}", left, right),
+            Sub(left, right) => write!(f, "{} - {}", left, right),
+            Mul(left, right) => write!(f, "{} * {}", left, right),
+            LessThan(left, right) => write!(f, "{} < {}", left, right),
+        }
+    }
 }
